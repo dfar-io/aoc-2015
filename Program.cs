ecea2f373f9109a081308ebabb59969f2963743e
Dave Farinelli <d@dfar.io>
2022-01-06 17:36:34 +0000

    Solves P01, both parts

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using AdventOfCode.Problems;
+
+Console.WriteLine($"Argument: {args[0]}");
+
+var problem = new P01(File.ReadAllLines("input.txt")[0]);
+
+Console.WriteLine($"Answer 1: {problem.Answer1}");
+Console.WriteLine($"Answer 2: {problem.Answer2}");
