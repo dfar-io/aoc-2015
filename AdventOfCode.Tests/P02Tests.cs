1e2f3fcc9cef845f3f37e7d4e7a5bb6ab5c19ce6
Dave Farinelli <d@dfar.io>
2022-01-18 16:39:52 +0000

    Fixes CodeClimate maint. issues

--- AdventOfCode.Tests/P02Tests.cs
+++ AdventOfCode.Tests/P02Tests.cs
@@ -7,18 +7,12 @@ namespace AdventOfCode.Tests;
 public class P02Tests
 {
     [Test]
-    [TestCase("2x3x4", 58)]
-    [TestCase("1x1x10", 43)]
-    public void Box_SurfaceAreaPlus_Works(string input, int answer)
+    [TestCase("2x3x4", 58, 34)]
+    [TestCase("1x1x10", 43, 14)]
+    public void Box_Works(string input, int surfaceArea, int ribbon)
     {
-        new Box(input).SurfaceAreaPlus().Should().Be(answer);
-    }
-
-    [Test]
-    [TestCase("2x3x4", 34)]
-    [TestCase("1x1x10", 14)]
-    public void Box_Ribbon_Works(string input, int answer)
-    {
-        new Box(input).Ribbon().Should().Be(answer);
+        var box = new Box(input);
+        box.SurfaceAreaPlus().Should().Be(surfaceArea);
+        box.Ribbon().Should().Be(ribbon);
     }
 }
--- AdventOfCode.Tests/P03Tests.cs
+++ AdventOfCode.Tests/P03Tests.cs
@@ -7,20 +7,14 @@ namespace AdventOfCode.Tests;
 public class P03Tests
 {
     [Test]
-    [TestCase(">", 2)]
-    [TestCase("^>v<", 4)]
-    [TestCase("^v^v^v^v^v", 2)]
-    public void P03P1_Works(string input, int answer)
+    [TestCase(">", 2, 2)]
+    [TestCase("^v", 2, 3)]
+    [TestCase("^>v<", 4, 3)]
+    [TestCase("^v^v^v^v^v", 2, 11)]
+    public void P03_Works(string input, int answer1, int answer2)
     {
-        new P03(input).Answer1.Should().Be(answer);
-    }
-
-    [Test]
-    [TestCase("^v", 3)]
-    [TestCase("^>v<", 3)]
-    [TestCase("^v^v^v^v^v", 11)]
-    public void P03P2_Works(string input, int answer)
-    {
-        new P03(input).Answer2.Should().Be(answer);
+        var problem = new P03(input);
+        problem.Answer1.Should().Be(answer1);
+        problem.Answer2.Should().Be(answer2);
     }
 }
