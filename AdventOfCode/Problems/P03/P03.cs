5d75aae89ecb3874acd91d1d2f5543748362742e
Dave Farinelli <d@dfar.io>
2022-03-15 00:11:20 +0000

    P03 - reducing complexity

--- AdventOfCode/Problems/P03/P03.cs
+++ AdventOfCode/Problems/P03/P03.cs
@@ -6,7 +6,7 @@ public class P03 : Problem
 
     public P03(string[] input) : base(input)
     {
-        (int x, int y) currentCoords = (0,0);
+        (int x, int y) p1Coords = (0,0);
         (int x, int y) santaCoords = (0,0);
         (int x, int y) roboSantaCoords = (0,0);
         var visitedLocations = new HashSet<(int x, int y)>();
@@ -14,42 +14,22 @@ public class P03 : Problem
         var roboSantaVisitedLocations = new HashSet<(int x, int y)>();
         var isSantaRound = true;
 
-        visitedLocations.Add(currentCoords);
-        santaVisitedLocations.Add(currentCoords);
-        roboSantaVisitedLocations.Add(currentCoords);
+        visitedLocations.Add(p1Coords);
+        santaVisitedLocations.Add(p1Coords);
+        roboSantaVisitedLocations.Add(p1Coords);
 
         foreach (var direction in input[0])
         {
-            // I'm guessing this is the problem
             var p2Coords = isSantaRound ? santaCoords : roboSantaCoords;
+            HandleDirection(ref p1Coords, direction, ref p2Coords);
 
-            switch (direction)
-            {
-                case '^':
-                    currentCoords.y += 1;
-                    p2Coords.y += 1;
-                    break;
-                case '<':
-                    currentCoords.x -= 1;
-                    p2Coords.x -= 1;
-                    break;
-                case '>':
-                    currentCoords.x += 1;
-                    p2Coords.x += 1;
-                    break;
-                case 'v':
-                    currentCoords.y -= 1;
-                    p2Coords.y -= 1;
-                    break;
-            }
-
-            visitedLocations.Add(currentCoords);
+            visitedLocations.Add(p1Coords);
             if (isSantaRound)
             {
                 santaCoords = p2Coords;
                 santaVisitedLocations.Add(santaCoords);
             }
-            else 
+            else
             {
                 roboSantaCoords = p2Coords;
                 roboSantaVisitedLocations.Add(roboSantaCoords);
@@ -60,5 +40,28 @@ public class P03 : Problem
 
         Answer1 = visitedLocations.Count();
         Answer2 = santaVisitedLocations.Union(roboSantaVisitedLocations).Count();
+    }
+
+    private static void HandleDirection(ref (int x, int y) p1Coords, char direction, ref (int x, int y) p2Coords)
+    {
+        switch (direction)
+        {
+            case '^':
+                p1Coords.y += 1;
+                p2Coords.y += 1;
+                break;
+            case '<':
+                p1Coords.x -= 1;
+                p2Coords.x -= 1;
+                break;
+            case '>':
+                p1Coords.x += 1;
+                p2Coords.x += 1;
+                break;
+            case 'v':
+                p1Coords.y -= 1;
+                p2Coords.y -= 1;
+                break;
+        }
     }
 }
